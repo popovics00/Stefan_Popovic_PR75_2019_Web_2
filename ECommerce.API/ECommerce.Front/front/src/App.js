09466bc3b5be69d297ced88ef9823ab3d9b6db10
Stefan Popović <stefan.popowic00@gmail.com>
2023-05-24 17:45:29 +0200

    temp design of page

--- ECommerce.API/ECommerce.Front/front/src/App.js
+++ ECommerce.API/ECommerce.Front/front/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/login";
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/register";
+import Dashboard from "./components/dashboard";
+import Navigation from "./components/navigation-comp/navigation";
+import AddArticle from "./components/article-comp/addArticle";
+import MainShop from "./components/mainShop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-container">
+        <Navigation />
+        <Routes>
+          <Route path="" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="add-article" element={<AddArticle />} />
+          <Route path="main-shop" element={<MainShop />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
